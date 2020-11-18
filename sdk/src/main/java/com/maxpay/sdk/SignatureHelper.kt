e92abb38c9df5ef46202909ee4e023f4dc23c416
Jack Krasnik <a.polyakh@dinarys.com>
2020-11-18 12:12:54 +0200

    MXM-59 Add public method to SignatureHelper.kt

--- sdk/src/main/java/com/maxpay/sdk/SignatureHelper.kt
+++ sdk/src/main/java/com/maxpay/sdk/SignatureHelper.kt
@@ -10,23 +10,47 @@ class SignatureHelper {
 
     private val privateKey = "sklive_wbkz4pc670ajfywc9st0ioajc07cesok"
 
-//    fun getHashOfRequest(
-//        api_version: Int?,
-//        auth_type: String? = "bysignature",
-//        transaction_type: TransactionType,
-//        currency: String?,
-//        first_name: String?,
-//        last_name: String?,
-//        address: String?,
-//        city: String?,
-//        state: String?,
-//        zip: String?,
-//        country: String?,
-//        user_phone: String?,
-//
-//
-//    ): String {
-//    }
+    fun getHashOfRequest(
+        api_version: Int?,
+        auth_type: String? = "bysignature",
+        transaction_type: TransactionType,
+        transaction_unique_id: String?,
+        currency: String?,
+        first_name: String,
+        last_name: String,
+        address: String?,
+        city: String?,
+        state: String?,
+        zip: String?,
+        country: String?,
+        user_phone: String?,
+        user_email: String?,
+        user_ip: String?,
+        callback_url: String?,
+        redirect_url: String?,
+        amount: Float
+    ): String {
+        val sigH = SignatureH(
+            api_version = api_version,
+            auth_type = auth_type?.takeIf { it != null }?: "bysignature",
+            transaction_unique_id = transaction_unique_id,
+            transaction_type = transaction_type.toString(),
+            currency = currency,
+            first_name = first_name,
+            last_name = last_name,
+            address = address,
+            city = city,
+//            state = request.state,
+            zip = zip,
+            country = country,
+            user_phone = user_phone,
+            user_email = user_email,
+            user_ip = user_ip,
+            callback_url = callback_url,
+            redirect_url = redirect_url
+        )
+        return calculateHash(sigH, amount)
+    }
 
     internal fun getHashOfRequest(request: SalePayment): String {
         val sigH = SignatureH(
@@ -48,8 +72,11 @@ class SignatureHelper {
             callback_url = request.callBackUrl,
             redirect_url = request.redirectUrl
         )
+        return calculateHash(sigH, request.amount)
+    }
 
-        sigH.amount = request.amount.removeZero()
+    private fun calculateHash(sigH: SignatureH, amount: Float): String {
+        sigH.amount = amount.removeZero()
         var str = ""
         val arr = arrayListOf<String>()
         for (prop in SignatureH::class.memberProperties) {
